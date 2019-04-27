0828f93592592fb2ae4c9d830eb6873295ded959
Ragheed Huneineh <ragheedmhuneineh@outlook.com>
2019-04-27 18:12:30 +0200

    Finishing Calculations

--- HRR.m
+++ HRR.m
@@ -24,7 +24,7 @@ qm_f = 4.072; % [kg/h] Fuel mass flow rate
 qm_egr = 9.533991; % [g/s]
 cp_egr = 1.038*1e3; % [J/(kg.K)]
 R_a = 287.05; % [J/kg]
-Hip = 44.5*1e6; % [J/kg]
+Hip = 44.5; % [J/kg]
 %% Set Parameters
 lambda = cr/crl;
 Vd = pi*bore^2/4*(2*cr); % [m^3]
@@ -33,16 +33,60 @@ area = pi*bore^2/4;
 x = cr*((1-cosd(theta)) + 1/lambda*(1-sqrt(1-lambda^2*sind(theta).^2))) + Vc/area; % [m] piston position
 V = area*x; % [m^3] cylinder volume vs theta
 
-%% Calculation
-dp(1) = (p_cyl1(1) - p_cyl1(end))*1e5; % [Pa] delta_P
-dV(1) = V(1) - V(end); % [m^3] delta_V
-for i = 2:length(p_cyl1)
-   dp(i) = (p_cyl1(i) - p_cyl1(i-1))*1e5; % [Pa] 
-   dV(i) = V(i) - V(i-1); % [m^3]
+%% Temperature and Elastic Constant
+m_a = qm_a/60/nc/rpm; % [kg/(cycle.cylinder)] Air mass
+m_f = 2*qm_f/60/nc/(rpm*2*pi/60); % [kg/(cycle.cylinder)] Fuel mass flow rate
+m_egr = qm_egr*60/1000/rpm; % [kg/rad.cylinder)] EGR mass
+R_egr = cp_egr*(1-1/1.4); % [J/(kg.K)] EGR elastic constant
+T_cyl = p_cyl1*1e5.*V./(m_a*R_a + m_egr*R_egr) + 273; % [K] in-cylinder temperature
+K = 1.338 - 6*1e-5*T_cyl + 1e-8*T_cyl.^2; % [-] Polytropic Coefficient
+
+%% Graphically
+SOI = 348.7; % [deg] Start Of Injection
+SOC = 358; % [deg] Start Of Combustion
+
+%% Pressure
+m = 1.45; % [-] Compression Polytropic Coefficient
+mp = 1.3; % [-] Expansion Polytropic Coficient
+Vt = Vd + Vc;
+% Intake (7 ->1)
+P(1:1801) = min(p_cyl1);
+
+% Compression (1 -> 2)
+V12 = V(1802:3601);
+P(1802:3601) =  P(1801)*(Vt./V12).^m;
+
+% Expansion (3 -> 4)
+V34 = V(3602:5402);
+P(3602:5402) = P(3601)*(Vc./V34).^mp;
+
+P(5403:7200) = P(5402);
+P(theta<=SOC) = p_cyl1(theta<=SOC);
+P(theta>SOC) = P(theta==SOC)*(V(theta==SOC)./V(theta>SOC)).^mp;
+%% Mass Fraction Burned
+for i = 2:length(theta)
+       if(theta(i) <= SOI)
+          Vdp(i) = 0;
+          pdV(i) = 0;
+       else
+          Vdp(i) = trapz([p_cyl1(i-1) p_cyl1(i)], [V(i-1) V(i)]/(K(i)-1)) + Vdp(i-1);
+          pdV(i) = trapz([V(i-1) V(i)], [p_cyl1(i-1) p_cyl1(i)]*K(i)/(K(i)-1)) + pdV(i-1);
+       end
 end
-dp = dp';
-dV = dV';
-m_a = qm_a/60/(nc*rpm/2); % [kg/(cycle.cylinder)] Air mass flow rate
-m_egr = qm_egr*60/1000/(nc*rpm/2); % [kg/cycle.cylinder)] Fuel mass flow rate
-R_egr = cp_egr*(1-1/1.4); % [J/(kg.K)]
-T_cyl = p_cyl1*1e5.*V./(m_a*R_a + m_egr*R_egr); 
+dQ_ch = Vdp + pdV;
+xb = dQ_ch/m_f/Hip;
+
+figure(1)
+hold on
+plot(theta(theta>=320 & theta<=430), p_cyl1(theta>=320 & theta<=430))
+plot(theta(theta>=320 & theta<=430), P(theta>=320 & theta<=430))
+plot(theta(theta>=320 & theta<=430), inj1(theta>=320 & theta<=430))
+ylim([0 max(p_cyl1)])
+yyaxis right
+plot(theta(theta>=320 & theta<=430), xb(theta>=320 & theta<=430), ':', 'LineWidth', 1.5)
+ylim([0 1])
+plot([SOI SOI], [0 1], 'm--')
+plot([SOC SOC], [0 1], 'k--')
+plot([362.18 362.18], [0 1], 'g--')
+legend('PCYL1', 'p_m_o_t_o_r_e_d', 'INJ1', 'x_b')
+grid on
